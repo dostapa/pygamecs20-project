57581fdc6600818047afb1066dd21d8dd08ee9a0
Dylan Ostapa <dylan.ostapa@RBE.sk.ca>
2019-06-12 14:06:50 -0600

    [

--- main.py
+++ main.py
@@ -31,9 +31,8 @@ pygame.init()
 pygame.font.init()
 #sets font type
 font = pygame.font.SysFont('Comic Sans MS', 25)
-font2 = pygame.font.SysFont('Comic Sans MS', 75)
 #window caption
-pygame.display.set_caption('Oh No')
+pygame.display.set_caption('YEe HaW') 
 #sets the screen to the screen variable and defines the screen width and height.
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 done = False
@@ -51,15 +50,14 @@ bullet = bullet()
 enemies.add(enemy1)
 playerg.add(player)
 enemies.add(enemy1)
-#self explanitory
-backgroundcolor = (20,20,20)
+backgroundcolor = (15,15,15)
 #variables
-php = (0,255,0)
+phc = (0,255,0)
 phh = 150
 bx = 0
 by = 0
 enemycount = 40
-level = 1
+level = 0
 #random enemy initial spawn
 enemy1.rect.x = random.randint(1,1920)
 enemy1.rect.y = random.randint(1,1080)
@@ -68,17 +66,21 @@ pygame.mouse.set_cursor(*pygame.cursors.broken_x)
 #movement rate of the enemy
 emoverate = 1
 emoverateadd = 0.05
+
 #Main game loop
 while not done:
         for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                         done = True
+
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     #checks for a click and sets the bullet there
                     bx, by = event.pos
                     bullets.add(bullet)
                     bullet.rect.x = bx
                     bullet.rect.y = by
+
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pygame.sprite.Sprite.kill(bullet)
                     bullet.rect.x = 2000
@@ -112,11 +114,13 @@ while not done:
                 pygame.sprite.Sprite.kill(enemy1)
                 enemycount -= 1;
                 phh += 2.5
+
                 if enemycount <= 0:
                     level += 1
                     enemycount = 40
-                    emoverateadd += 0.05
+                    emoverateadd += 0.02
                     phh += 50
+
                 emoverate += emoverateadd
                 enemies.add(enemy1)
                 enemy1.rect.x = random.randint(1,1920)
@@ -130,6 +134,7 @@ while not done:
             done = True
 
         if player.rect.x != enemy1.rect.x:
+
             if player.rect.x > enemy1.rect.x:
                 enemy1.rect.x += emoverate
 
@@ -137,6 +142,7 @@ while not done:
                 enemy1.rect.x -= emoverate
 
         if player.rect.y != enemy1.rect.y:
+
             if player.rect.y > enemy1.rect.y:
                 enemy1.rect.y += emoverate
 
@@ -144,32 +150,37 @@ while not done:
                 enemy1.rect.y -= emoverate
 
         #updates the sprite groups
+
         bullets.update()
         enemies.update()
         playerg.update()
+
         #Fills the screen every time the screen is updated (makes it so the square doesn't leave a ghost behind)
 
         screen.fill(backgroundcolor)
         #draws healthbar for player
 
-        pygame.draw.rect(screen, php, pygame.Rect(40, 20, 20, phh))
+        pygame.draw.rect(screen, phc, pygame.Rect(40, 20, 20, phh))
         #draws the text bars displaying enemy's left and enemy speed
+
         textsurface = font.render("Enemy's Left: {0}".format(enemycount), False, (255, 255, 255))
         textsurface2 = font.render("Enemy's Speed: {0}".format(emoverate), False, (255, 255, 255))
         textsurface4 = font.render("Level: {0}".format(level), False, (255, 255, 255))
-        if level == 3:
-            textsurface3 = font2.render("You Win", False, (255, 255, 255))
-            screen.blit(textsurface3,(960,540))
-            
+
         #draws the text onto the screen
+
         screen.blit(textsurface,(200,0))
         screen.blit(textsurface2,(200,50))
         screen.blit(textsurface4,(200,100))
+
         #draws the sprites on the screen
+
         bullets.draw(screen)
         enemies.draw(screen)
         playerg.draw(screen)
+
         #Causes the screen to show.
+
         pygame.display.flip()
         #fps
         clock.tick(60)
